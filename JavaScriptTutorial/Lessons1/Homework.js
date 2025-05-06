803ca5ef3c0a3f84ca35e7332fc35b045d532640
yangqiong520 <3441723843@qq.com>
2025-05-06 16:04:57 +0800

    家庭作业1

--- JavaScriptTutorial/Lessons1/Homework.js
+++ JavaScriptTutorial/Lessons1/Homework.js
@@ -8,17 +8,17 @@ var d = "3.14abc";
 var e = undefined;
 
 // 以下表达式的结果是什么？
-console.log(Number(a));     // ?
-console.log(parseInt(d));   // ?
-console.log(String(b));     // ?
-console.log(typeof c);      // ?
-console.log(isNaN(d));      // ?
+console.log(Number(a));     // ? 123
+console.log(parseInt(d));   // ? NaN
+console.log(String(b));     // ? 1
+console.log(typeof c);      // ? number
+console.log(isNaN(d));      // ? flase
 
 // 以下加法运算的结果是什么？
-console.log(a + b);         // ?
-console.log(b + c);         // ?
-console.log(c + d);         // ?
-console.log(d + e);         // ?
+console.log(a + b);         // ? '1231'
+console.log(b + c);         // ? 1 
+console.log(c + d);         // ? 'null3.14abc'
+console.log(d + e);         // ? '3.14abcundefined' 
 
 
 
@@ -30,13 +30,13 @@ var y = 100;
 var z = "50px";
 
 // 以下表达式的结果是什么？
-console.log(typeof x);      // ?
-console.log(typeof y);      // ?
-console.log(parseInt(z));   // ?
-console.log(isNaN(z));      // ?
-console.log(String(y));     // ?
+console.log(typeof x);      // ? string
+console.log(typeof y);      // ? number
+console.log(parseInt(z));   // ? NaN
+console.log(isNaN(z));      // ? flase
+console.log(String(y));     // ? "100"
 
 // 执行以下操作后，变量的值和类型是什么？
-var result1 = x + y;            // result1的值和类型是？
-var result2 = y + Number(z);    // result2的值和类型是？
-var result3 = y + parseInt(z);  // result3的值和类型是？
+var result1 = x + y;            // result1的值和类型是？ number = x + y  number
+var result2 = y + Number(z);    // result2的值和类型是？ number = y + Number(z)  string||bolean
+var result3 = y + parseInt(z);  // result3的值和类型是？ number = y + parseInt(z)  string
