20ced216fb250a5743d8e5da165e2422960e8aa8
Siweichengoluy <3533804443@qq.com>
2025-12-01 19:31:11 +0800

    test: 修改测试文件

--- JavaScriptTutorial/Lessons1/Homework.js
+++ JavaScriptTutorial/Lessons1/Homework.js
@@ -1,19 +1,19 @@
 /*
 一、选择题
 
-1. 以下哪个是合法的变量名？ （  ）
+1. 以下哪个是合法的变量名？ （ B ）
   A. 123name
   B. var
   C. _age
   D. first name
 
-2. 以下关于变量的描述错误的是： （  ）
+2. 以下关于变量的描述错误的是： （ B ）
   A. 变量名区分大小写
   B. 变量可以重复定义不会覆盖
   C. 变量名不能以数字开头
   D. 变量是存储数据的容器
 
-3. isNaN("100")的返回结果是： （  ）
+3. isNaN("100")的返回结果是： （ B ）
   A. true
   B. false
   C. "100"
@@ -28,7 +28,7 @@
 var nameA = "张三";
 nameA = "李四";
 console.log(nameA);
-// 结果是：
+// 结果是：李四
 
 
 // 2.写出以下代码的输出结果：
@@ -36,7 +36,8 @@ var a = 10;
 var b = "20";
 console.log(typeof a);
 console.log(typeof b);
-// 结果是：
+// 结果是：number
+string
 
 
 // 3. 写出以下代码的输出结果：
@@ -44,7 +45,7 @@ var x;
 console.log(typeof x);
 x = null;
 console.log(typeof x);
-// 结果是：
+// 结果是：null
 
 
 /*
@@ -55,8 +56,9 @@ var isNumber1 = 123
 var isNumber2 = "hello"
 var isNumber3 = "456"
 // 第一题代码写在这里：
-
-
+console.log(typeof isNumber1) = number
+console.log(typeof isNumber2) = string
+console.log(typeof isNumber3) = string
 
 
 
@@ -68,3 +70,9 @@ var isNumber3 = "456"
 // 已知：长方形的长是20，宽是10，有8个这样的长方形
 // 请计算：总长方形的周长和面积，并输出结果
 // 第二题代码写在这里：
+var 1 =20
+var 2 =10
+var 3 =8
+var 4 =2
+console.log((1+2)*4*3) =60
+console.log(1*2*3) =1600
