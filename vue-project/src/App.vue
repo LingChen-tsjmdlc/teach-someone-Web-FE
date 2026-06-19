61d5abf183fd07d76d0396b6497f10e17df3441c
LingChen-tsjmdlc <3024705530@qq.com>
2026-06-19 17:54:20 +0800

    vue 讲解

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,18 +1,15 @@
-<script setup lang="ts"></script>
-
 <template>
- <div>
- <p>当前计数：{{count}}</p>
- <button @click="addOne">点击+1</button>
- </div>
- </template>
- 
- <script setup>
- import{ref}from'vue';
-const count=ref(0);
-const addOne=()=>{
-  count.value++;
+  <div>
+    <p class="bg-blue-300" :class="{ 'text-red-500': count >= 10 }">当前计数：{{ count }}</p>
+    <button @click="addOne">点击+1</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+const addOne = () => {
+  count.value = count.value + 1
 }
 </script>
-
-<style scoped></style>
