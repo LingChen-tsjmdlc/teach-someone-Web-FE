31d3c6d6e1fda539faa37852a3bbb1dbbd2f8700
VBlkbnj <2831029729@qq.com>
2026-06-26 22:38:14 +0800

    homework:组件props实践

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -3,10 +3,14 @@
     <p class="bg-blue-300" :class="{ 'text-red-500': count >= 10 }">当前计数：{{ count }}</p>
     <button @click="addOne">点击+1</button>
   </div>
+  <div class="flex flex-row">
+    <card addFirst="bg-yellow-500" addSecond="bg-blue-300" /><card /><card />
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import card from './components/card.vue'
 
 const count = ref(0)
 const addOne = () => {
---
+++ vue-project/src/components/card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="w-64 rounded-xl shadow-lg overflow-hidden hover:scale-110 transition-transform duration-300 ease-in-out"
+  >
+    <!-- 上半部分 -->
+    <div :class="'bg-blue-500 p-4 text-white h-15 ' + addFirst">
+      <h2 class="text-xl font-bold">卡片标题</h2>
+    </div>
+    <!-- 下半部分 -->
+    <div :class="'bg-white p-4 text-gray-800 h-80 ' + addSecond">
+      <p>这里是卡片的下半部分内容，可以放置描述文字或操作按钮。</p>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+defineProps({
+  addFirst: String,
+  addSecond: String,
+})
+</script>
